5f8554ae52797d187229ad13dc6d393c9b86b50a
lliolla <lliolla@hotmail.com>
2023-04-20 23:10:06 +0200

    FEAT: ajout page about

--- src/pages/Apropos.jsx
+++ src/pages/Apropos.jsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React from "react";
+import Mouse from "../components/Mouse";
+import Nav from "../components/Nav";
+import Logo from "../components/Logo";
 
 const Apropos = () => {
-    return (
-        <div>
-            Développeuse web  passionnée par le monde digitale , je suis à la recherche d'un nouveau projet passionnant a partager avec vos équipes
+  return (
+    <main>
+      <Mouse />
+      <div className="about">
+        <Nav />
+        <Logo />
+        <div className="container">
+        <div className="about-container">
+          <h2>A propos</h2>
+          <p>
+            Développeuse web passionnée par le monde digital, je suis à la
+            recherche d'un nouveau projet passionnant a partager avec vos
+            équipes
+          </p>
         </div>
-    );
+        <div className="stack-container">
+          <h2>Ma stack</h2>
+          <div className="stack-icons">
+            <div>
+    
+              <img src="/public/assets/logo/node.png" alt="" />
+            </div>
+            <div>
+              <img src="/public/assets/logo/js.png" alt=""  />
+            </div>
+            <div>
+    
+              <img src="/public/assets/logo/react.png" alt="" />
+            </div>
+            <div>
+    
+              <img src="/public/assets/logo/vuejs.png" alt="" />
+            </div>
+            <div>
+    
+              <img src="/public/assets/logo/sql.png" alt="" style={{width:"100%"}}/>
+            </div>
+          </div>
+        </div>
+        </div>
+        
+      </div>
+    </main>
+  );
 };
 
 export default Apropos;
